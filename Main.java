c37cb51d3fcd57f6fc15da0d713227c3de07574c
Mamady Sillah <supurpisces310@msn.com>
2020-10-28 14:24:24 -0400

    augmentd assignmentn operators

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     //access modifier keyword to determine what classes and can access methods and attributes
     public static void main(String[] args) {
     int x = 1;
-    x++;
+    x -= 2;
     System.out.println(x);
     
 
