e16735113fda69bda1176e773a0b5bdb81ceaec2
Mamady Sillah <supurpisces310@msn.com>
2020-10-28 14:22:33 -0400

    increment operator

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     //access modifier keyword to determine what classes and can access methods and attributes
     public static void main(String[] args) {
-    final float PI = 3.14F;
-    PI = 3.5;
-    System.out.println(PI);
+    int x = 1;
+    x++;
+    System.out.println(x);
     
 
 }
