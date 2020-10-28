c65f1368082610cd6946b80ff986ecc54aaeed25
Mamady Sillah <supurpisces310@msn.com>
2020-10-28 14:17:47 -0400

    use the final keyword before declaring a variable to make it a constant

--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     //access modifier keyword to determine what classes and can access methods and attributes
     public static void main(String[] args) {
-   int [][] numbers = {{1, 2, 3}, {4, 5, 6}};
-   System.out.println(Arrays.deepToString(numbers));
+    final float PI = 3.14F;
+    PI = 3.5;
+    System.out.println(PI);
+    
 
 }
 }
