d4ab75e7315a84aa6c6429b6b745484a01729de9
Mamady Sillah <supurpisces310@msn.com>
2020-10-28 17:50:41 -0400

    instead of println do print so that the input section will come after age

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
  
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
-      System.out.println("Age: ");
+      System.out.print("Age: ");
       byte age = scanner.nextByte();
       System.out.println("you are " + age);
 
