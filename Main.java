be12661d3e23a2ac7373e1f8be0f764a2192ae2c
Mamady Sillah <supurpisces310@msn.com>
2020-12-15 22:13:17 -0500

    java while loop

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
    
     public static void main(String[] args) {
-        int number; 
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
 
-        for ( number = 1; number < 21; number++) {
-        if (number % 3 == 0 && number % 5 ==0) {
-            System.out.println("Fizz Buzz");
-        } else if (number % 5 == 0) {
-            System.out.println("Fizz");
-        } else if ( number % 3 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(number);
-        }
-    }
+        while (!input.equals("quit")) {
+        System.out.print("Input: ");
+        input = scanner.next().toLowerCase();
+        System.out.println(input);
   
     } 
+}
+
 }
