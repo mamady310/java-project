d6f3a770f3441b164691a174996052e18fec7643
Mamady Sillah <supurpisces310@msn.com>
2020-12-15 19:32:40 -0500

    if stmt

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+   
+    public static void main(String[] args) {
+     
+        int temp = 32;
+        if( temp > 30) {
+        System.out.println("its a hot day");
+        } else if (temp > 20) {
+        System.out.println("Beautiful day"); 
+        } else {
+        System.out.println("cold da");
+        }
+
+
+    } 
+}
