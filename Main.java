4fab89d175ce7883e9a2407e2c6e4676f296e692
Mamady Sillah <supurpisces310@msn.com>
2020-12-16 19:30:38 -0500

    break statements in while loop

--- Main.java
+++ Main.java
@@ -6,19 +6,16 @@ public class Main {
         String input = "";
         Scanner scanner = new Scanner(System.in);
 
-    //     while (!input.equals("quit")) {
-    //     System.out.print("Input: ");
-    //     input = scanner.next().toLowerCase();
-    //     System.out.println(input);
-  
-    // } 
-
-    do {
+        while (!input.equals("quit")) {
         System.out.print("Input: ");
         input = scanner.next().toLowerCase();
-        System.out.println(input);  
+        if (input.equals("quit"))
+            break;
+        System.out.println(input);
+  
+    } 
 
-    } while (!input.equals("quit"));
+}
 }
 
-}
+
